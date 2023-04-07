653eba25ae9c29248a1e1c60c8411c46ea46bff6
Katherine Fortin <kafortin@student.42quebec.com>
2023-04-07 17:47:58 -0400

    adjusted the random moving of the fox so it is more random

--- bonus/ennemy_bonus.c
+++ bonus/ennemy_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 17:18:25 by kafortin          #+#    #+#             */
-/*   Updated: 2023/04/07 17:30:10 by kafortin         ###   ########.fr       */
+/*   Updated: 2023/04/07 17:46:48 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,57 @@
 
 void	move_fox_horizontally(t_game *game, int random)
 {
-	put_image(game, game->sprite.base, game->fox.x, game->fox.y);
-	game->map[game->fox.x][game->fox.y] = '0';
-	game->fox.y = game->fox.y + random;
-	game->map[game->fox.x][game->fox.y] = 'F';
-	if (random < 0)
-		put_image(game, game->sprite.fox_left, game->fox.x, game->fox.y);
-	else
-		put_image(game, game->sprite.fox, game->fox.x, game->fox.y);
+	if (game->map[game->fox.x][game->fox.y + random] == '0')
+	{
+		put_image(game, game->sprite.base, game->fox.x, game->fox.y);
+		game->map[game->fox.x][game->fox.y] = '0';
+		game->fox.y = game->fox.y + random;
+		game->map[game->fox.x][game->fox.y] = 'F';
+		if (random < 0)
+			put_image(game, game->sprite.fox_left, game->fox.x, game->fox.y);
+		else
+			put_image(game, game->sprite.fox, game->fox.x, game->fox.y);
+	}
+	else if (game->map [game->fox.x][game->fox.y + random] == 'P')
+		game_over(game);
 }
 
 void	move_fox_vertically(t_game *game, int random)
 {
-	put_image(game, game->sprite.base, game->fox.x, game->fox.y);
-	game->map[game->fox.x][game->fox.y] = '0';
-	game->fox.x = game->fox.x + random;
-	game->map[game->fox.x][game->fox.y] = 'F';
-	if (random < 0)
-		put_image(game, game->sprite.fox_up, game->fox.x, game->fox.y);
-	else
-		put_image(game, game->sprite.fox_down, game->fox.x, game->fox.y);
+	if (game->map[game->fox.x + random][game->fox.y] == '0')
+	{
+		put_image(game, game->sprite.base, game->fox.x, game->fox.y);
+		game->map[game->fox.x][game->fox.y] = '0';
+		game->fox.x = game->fox.x + random;
+		game->map[game->fox.x][game->fox.y] = 'F';
+		if (random < 0)
+			put_image(game, game->sprite.fox_up, game->fox.x, game->fox.y);
+		else
+			put_image(game, game->sprite.fox_down, game->fox.x, game->fox.y);
+	}
+	else if (game->map [game->fox.x + random][game->fox.y] == 'P')
+		game_over(game);
 }
 
 int	fox_hook(t_game *game)
 {
-	int	random_x;
-	int	random_y;
+	int	random_direction;
+	int	random_value;
 	int	speed;
 
 	game->loop++;
-	random_x = (rand() % 3) - 1;
-	random_y = (rand() % 3) - 1;
+	random_value = (rand() % 2);
+	random_direction = (rand() % 2);
+	if (random_direction == 0)
+		random_direction = -1;
 	speed = 5555;
 	game->ennemy = game->loop % speed;
 	if (game->ennemy == 0 && game->status != OVER)
 	{
-		if (game->map[game->fox.x][game->fox.y + random_y] == '0')
-			move_fox_horizontally(game, random_y);
-		else if (game->map [game->fox.x][game->fox.y + random_y] == 'P')
-			game_over(game);
-		else if (game->map[game->fox.x + random_x][game->fox.y] == '0')
-			move_fox_vertically(game, random_x);
-		else if (game->map [game->fox.x + random_x][game->fox.y] == 'P')
-			game_over(game);
+		if (random_value == 0)
+			move_fox_horizontally(game, random_direction);
+		else if (random_value == 1)
+			move_fox_vertically(game, random_direction);
 	}
 	return (0);
 }
